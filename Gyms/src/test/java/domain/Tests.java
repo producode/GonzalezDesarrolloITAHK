458ce449a9567010aaa423222de2a8b40c265076
producode <produgraf2000@gmail.com>
2020-04-23 11:20:07 -0300

    se creo el software Tamagochi

---
+++ Gyms/src/test/java/domain/Tests.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+public class Tests {
+    private int nada = 0;
+    @Test
+    public void agregarPersona(){
+        Assert.assertEquals(true, true);
+    }
+}
